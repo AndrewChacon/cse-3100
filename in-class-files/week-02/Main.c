fd1c49e74239b7d648aa884a53d20806ee0b4120
AndrewChacon <andrew.b.chacon@gmail.com>
2024-09-05 11:56:19 -0400

    in class work for week 2 thursday

--- in-class-files/week-02/Main.c
+++ in-class-files/week-02/Main.c
@@ -3,6 +3,8 @@
 int main() {
     printf("Hello World!\n");
 
+    // september 3rd
+
     // read indefinte amount of values from user
     // compute running total and running average
     // stop when control c + control d
@@ -53,40 +55,106 @@ int main() {
 
     // PROBLEM #3
 
-    char operator;
-    double num1, num2, result;
+    // char operator;
+    // double num1, num2, result;
 
-    printf("Select an operation (+, -, *, /): ");
-    scanf("%c", &operator);
+    // printf("Select an operation (+, -, *, /): ");
+    // scanf("%c", &operator);
 
-    printf("Enter two numbers: ");
-    scanf("%lf %lf", &num1, &num2);
+    // printf("Enter two numbers: ");
+    // scanf("%lf %lf", &num1, &num2);
 
-    switch (operator)
-    {
-    case '+':
-        result = num1 + num2;
-        printf("%lf + %lf = %lf\n", num1, num2, result);
-        break;
-    case '-':
-        result = num1 - num2;
-        printf("%lf - %lf = %lf\n", num1, num2, result);
-        break;
-    case '*':
-        result = num1 * num2;
-        printf("%lf * %lf = %lf\n", num1, num2, result);
-        break;
-    case '/':
-        result = num1 / num2;
-        if(num2 == 0) {
-            printf("Invalid input for num2, cannot divide by 0 number.\n");
-        }
-        printf("%lf / %lf = %lf\n", num1, num2, result);
-        break;
-    default:
-        printf("Error: Invalid operator.\n");
-        break;
-    }
+    // switch (operator)
+    // {
+    // case '+':
+    //     result = num1 + num2;
+    //     printf("%lf + %lf = %lf\n", num1, num2, result);
+    //     break;
+    // case '-':
+    //     result = num1 - num2;
+    //     printf("%lf - %lf = %lf\n", num1, num2, result);
+    //     break;
+    // case '*':
+    //     result = num1 * num2;
+    //     printf("%lf * %lf = %lf\n", num1, num2, result);
+    //     break;
+    // case '/':
+    //     result = num1 / num2;
+    //     if(num2 == 0) {
+    //         printf("Invalid input for num2, cannot divide by 0 number.\n");
+    //         break;
+    //     }
+    //     printf("%lf / %lf = %lf\n", num1, num2, result);
+    //     break;
+    // default:
+    //     printf("Error: Invalid operator.\n");
+    //     break;
+    // }
+
+    // september 5th
+
+    // PROBLEM #1
+    // function to compute halleys sequence
+    // double x, epsillion;
+
+    // prinf("Enter the value x: ");
+    // scanf("%lf", &x);
+
+    // printf("Enter the type epsillon: ");
+    // scanf("%lf", &epsillion);
+
+    // double answer = halleys(x, epsillion);
+    // printf("Result: %lf", answer);
+
+
+    // PROBLEM #2
+
+    /* 
+    n discs, make disks from a to c - tower of hanoi problem
+    3 arrays
+    pushing the stack
+
+    using recurssion
+    */
+
+    int diskNumber;
+
+    printf("Enter the number of disks: ");
+    scanf("%d", diskNumber);
+
+    // using the move function calculate the number of moves needed
+    // print the result
+
+    printf("");
+
+    /*
+    1 move - a to c
+
+    1 move
+    
+    */
 
     return 0;
 }
+
+int move(int n, char a, char b, char c) {
+
+}
+
+// double halleys(double x, double epsillion) {
+//     // int guess = x / 2;
+//     // while(fabs(guess * guess - x) > epsillion) {
+
+//     // }
+
+//     double guess = x, prev_s = 0;
+//     guess = prev_s * (prev_s * prev_s + 3 * x) / (3 * prev_s * prev_s + x);
+
+//     while(fabs(guess - prev_s) >= epsillion) {
+//         prev_s = guess;
+//         guess = prev_s * (prev_s * prev_s + 3 * x) / (3 * prev_s * prev_s + x);
+//     }
+//     return guess;
+
+//     return 0;
+// }
