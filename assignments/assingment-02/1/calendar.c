38593c5e1a5c912fd3766b2ee3caadd1dfe38c0f
AndrewChacon <andrew.b.chacon@gmail.com>
2024-09-15 17:56:20 -0400

    assingment 2 files

--- assignments/assingment-02/1/calendar.c
+++ assignments/assingment-02/1/calendar.c
@@ -3,15 +3,28 @@
 
 int numDaysInMonth(int month, int year){
   switch(month){
+
   case JAN:
+  case MAR: 
+  case MAY:
+  case JUL:
+  case AUG:
+  case OCT:
+  case DEC:
     return 31;
-    break;
-     /*
-      TODO: Add the cases for February through December. Don't forget about leap years. 
-      For years that are not centuries (i.e. not 1900, 2000), it is a leap year if it is disible by 4.
-      For years that are centuries, it is a leap year if it is divisible by 400.
-      Return -1 if the month doesn't fit any of the cases. 
-     */
+
+  case APR:
+  case JUN:
+  case SEP:
+  case NOV:
+    return 30;
+
+  case FEB:
+    if((year % 4 == 0 && year % 100 != 0) || (year % 400 == 0)) {
+      return 29;
+    } else {
+      return 28;
+    }
   default:
     return -1;
   }
--- assignments/assingment-02/1/calendar.h
+++ assignments/assingment-02/1/calendar.h
@@ -10,9 +10,16 @@
 #define SATURDAY 6
 
 #define JAN 1
-/*
-  TODO: Define February through November here.
- */
+#define FEB 2
+#define MAR 3
+#define APR 4
+#define MAY 5
+#define JUN 6
+#define JUL 7
+#define AUG 8
+#define SEP 9
+#define OCT 10
+#define NOV 11
 #define DEC 12
 
 
