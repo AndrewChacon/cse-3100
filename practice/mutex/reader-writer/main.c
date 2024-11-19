bc1be03eceec536287f4208db45115e5007b0b21
AndrewChacon <andrew.b.chacon@gmail.com>
2024-11-19 00:28:43 -0500

    updated comments on problem for deeper understanding

--- practice/mutex/reader-writer/main.c
+++ practice/mutex/reader-writer/main.c
@@ -3,10 +3,12 @@
 #include <stdlib.h>
 #include <unistd.h>
 
+// controlling access to shared resource 
 pthread_mutex_t resource_mtx = PTHREAD_MUTEX_INITIALIZER;
+// controlling access to reader counter
 pthread_mutex_t reader_count_mtx = PTHREAD_MUTEX_INITIALIZER;
-// mutex for shared rsource and reader count
 
+// global variables 
 int reader_count = 0; // # of active readers
 int shared_resource = 0; // shared resource
 
@@ -17,7 +19,7 @@ void* reader(void*arg) {
     pthread_mutex_lock(&reader_count_mtx); // lock reader_count
     reader_count++;
     if(reader_count == 1) {
-        pthread_mutex_lock(&resource_mtx); // lock first reader
+        pthread_mutex_lock(&resource_mtx); // first reader locks shared resource 
     }
     pthread_mutex_unlock(&reader_count_mtx); // unlock reader count
 
@@ -31,8 +33,8 @@ void* reader(void*arg) {
     if(reader_count == 0) { // last reader unlocks the resource 
         pthread_mutex_unlock(&resource_mtx);
     }
-    pthread_mutex_unlock(&reader_count_mtx);
-    free(arg);
+    pthread_mutex_unlock(&reader_count_mtx); // unlocks after reader count is updated 
+    free(arg); // frees memory allocated for id 
     return NULL;
 }
 
@@ -50,31 +52,33 @@ void* writer(void* arg) {
     // writer exits
     pthread_mutex_unlock(&resource_mtx); // unlock shared resource
 
-    free(arg);
+    free(arg); // frees memory allocated for id
     return NULL;
 }
 
 int main(int argc, char* argv[]) {
-    pthread_t readers[5], writers[2];
+    pthread_t readers[5], writers[2]; // stores thread ids for reader and writer threads 
 
-    //create reader and writer threads
+    // create reader threads 
     for (int i = 0; i < 5; i++) {
         int *id = malloc(sizeof(int));
         *id = i + 1;
-        pthread_create(&readers[i], NULL, reader, id);
+        pthread_create(&readers[i], NULL, reader, id); // creates reader thread 
     }
 
+    // create writer threads
     for (int i = 0; i < 2; i++) {
         int *id = malloc(sizeof(int));
         *id = i + 1;
-        pthread_create(&writers[i], NULL, writer, id);
+        pthread_create(&writers[i], NULL, writer, id); //creates writer thread 
     }
 
-    // wait for threads to finish
+    // wait for all reader threads to finish
     for (int i = 0; i < 5; i++) {
         pthread_join(readers[i], NULL);
     }
 
+    // wait for all writer threads to finish
     for (int i = 0; i < 2; i++) {
         pthread_join(writers[i], NULL);
     }
